5c4d84165fb6f0d2030e1ee2991d0592c79309cf
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-01 17:58:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,22 +1,18 @@
 #include "lib.h"
-
 bool primo(char a){
 
-    if((a>=65 and a<=90) || (a>=97 and a<=127)){
+    if((a>=65 and a<=90)or(a>=97 and a<=127)){
         return true;
     }else{
         return false;
     }
 }
-
 char trasformazione(char a){
-    if (a>=65 a && a<=90){
+    if (a>=65 and a<=90){
         a=a+32;
         return a;
-    }else if(a>=97 && a<=127){
+    }else if(a>=97 and a<=127){
         a=a-32;
         return a;
     }
 }
-
-
